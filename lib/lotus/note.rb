048883b27383897b7543bb88f9a1a429e11aa93b
wilkie <wilkie05@gmail.com>
2013-06-19 03:45:03 -0400

    Adds #to_html to Lotus::Note.
    This method will produce html for the given content.
    
    There will need to be a way to resolve usernames to urls and produce a
    proper mentions list. Basically, a block passed in that can resolve a
    string username/domain to a Lotus::Author.

--- lib/lotus/note.rb
+++ lib/lotus/note.rb
@@ -1,5 +1,8 @@
 module Lotus
   class Note
+    # Determines what constitutes a username inside an update text
+    USERNAME_REGULAR_EXPRESSION = /(^|[ \t\n\r\f"'\(\[{]+)@([^ \t\n\r\f&?=@%\/\#]*[^ \t\n\r\f&?=@%\/\#.!:;,"'\]}\)])(?:@([^ \t\n\r\f&?=@%\/\#]*[^ \t\n\r\f&?=@%\/\#.!:;,"'\]}\)]))?/
+
     # Holds the content as plain text.
     attr_reader :text
 
@@ -38,13 +41,39 @@ module Lotus
     #   :uid          => The unique id that identifies this note.
     def initialize(options = {})
       @text      = options[:text] || ""
-      @html      = options[:html]
+      @html      = options[:html] || to_html
       @title     = options[:title] || "Untitled"
       @author    = options[:author]
       @url       = options[:url]
       @published = options[:published]
       @updated   = options[:updated]
       @uid       = options[:uid]
+    end
+
+    # Produces an HTML string representing the note's content.
+    # TODO: A block for resolving usernames to urls
+    def to_html
+      return @html if @html
+
+      out = CGI.escapeHTML(@text)
+
+      # Replace any absolute addresses with a link
+      # Note: Do this first! Otherwise it will add anchors inside anchors!
+      out.gsub!(/(http[s]?:\/\/\S+[a-zA-Z0-9\/}])/, "<a href='\\1'>\\1</a>")
+
+      # we let almost anything be in a username, except those that mess with urls.
+      # but you can't end in a .:;, or !
+      # also ignore container chars [] () "" '' {}
+      # XXX: the _correct_ solution will be to use an email validator
+      out.gsub!(USERNAME_REGULAR_EXPRESSION) do |match|
+        "#{$1}<a href='#'>@#{$2}</a>"
+      end
+
+      out.gsub!( /(^|\s+)#(\p{Word}+)/ ) do |match|
+        "#{$1}<a href='/search?search=%23#{$2}'>##{$2}</a>"
+      end
+
+      out
     end
 
     def to_hash
